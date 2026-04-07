ccdd5018877fa037124c3abaeadb4ee395b327ed
Mohamed EL Moumny <elmoumnymohamed1999@gmail.com>
2026-04-07 14:39:14 +0100

    feat: add cors config

--- backend/src/main/java/com/moeware/ims/config/SecurityConfig.java
+++ backend/src/main/java/com/moeware/ims/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.moeware.ims.security.JwtAuthenticationFilter;
 
@@ -33,6 +34,7 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Autowired
     @Lazy
@@ -41,6 +43,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         // api/auth/me need to be authenticated
